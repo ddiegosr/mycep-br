ede5a6d3828be72509c155b4c6f7e2a956b02a1f
Gabriel Ribeiro <gabriel.ribeiro.bastos@hotmail.com>
2018-09-30 12:36:50 -0300

    Criada a classe BuscaCEP

---
+++ src/mycep-br/BuscaCEP.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gabriel
+ * Date: 30/09/2018
+ * Time: 11:49
+ */
+
+class BuscaCEP
+{
+    private $url = "viacep.com.br/ws/{cep}/json/";
+
+    public function find($cep)
+    {
+        $url = $this->mountURL($cep);
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL,$url);
+        $result=curl_exec($ch);
+
+        curl_close($ch);
+
+        return json_decode($result, true);
+    }
+
+    private function mountURL($cep)
+    {
+        $cep = str_replace('-', '', $cep);
+        $url = $this->url;
+        $url = str_replace('{cep}', $cep, $url);
+
+        return $url;
+    }
+
+}
